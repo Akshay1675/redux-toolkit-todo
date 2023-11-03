35877e01b4ab8aba3b0d369ee6ea9a2ddca2e03a
Akshay Desai <112275432+Akshay1675@users.noreply.github.com>
2023-11-03 12:16:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import AddTodo from './components/AddTodo'
+import Todos from './components/todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>redux || toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
